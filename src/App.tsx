9e72ba4c3791dfe7af6a3b68ecedf7f14d6e2687
Ghaith-4381 <redmixi125g@gmail.com>
2025-06-10 17:18:59 +0300

    تعديلات لتحسين النقر الفوري

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -9,8 +8,6 @@ import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
 import AdminAds from "./pages/AdminAds";
 
-// داخل <Routes>
-
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -19,13 +16,12 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <Routes>"/admin"
+        <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin-4Bxr7Xt89-secure" element={<Admin />} />
+          <Route path="/admin-4Bxr7Xt89-secure/ads" element={<AdminAds />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-          <Route path="/admin-4Bxr7Xt89-secure/ads" element={<AdminAds />} />
-
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
